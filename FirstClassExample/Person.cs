d69c5452bced03be72724cc55f87d1090599d66f
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-08-04 16:07:30 +0300

    Add project files.

---
+++ FirstClassExample/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace FirstClassExample
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private DateOnly birthOfDate;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+
+        }
+        public DateOnly BirthOfDate
+        {
+            get { return birthOfDate; }
+            set { birthOfDate = value; }
+        }
+
+    }
+}
---
+++ FirstClassExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using FirstClassExample;
+
+Person teacher = new Person();
+Person teacher2  = new Person();
+teacher.FirstName = "Sertan";
+teacher.LastName = "Bozkuş";
+teacher.BirthOfDate = new DateOnly(1999,1,1);
+teacher2.FirstName = "Aleyna";
+teacher2.LastName = "Avcı";
+teacher2.BirthOfDate = new DateOnly(2000, 1, 1);
+
+
+Person student = new Person();
+Person student2 = new Person();
+student.FirstName = "Melike";
+student.LastName = "Demir Göz";
+student.BirthOfDate = new DateOnly(1992, 03, 17);
+student2.FirstName = "Ahmet";
+student2.LastName = "Göz";
+student2.BirthOfDate = new DateOnly(1992,09,18);
+
+
+Console.WriteLine("Öğretmen Bilgilerimiz  \n Ad Soyad: " + teacher.FirstName + " " + teacher.LastName + " \n Ad Soyad: " + teacher2.FirstName + " " + teacher2.LastName);
+
+Console.WriteLine("Öğrenci Bilgilerimiz \n Ad Soyad: "+student.FirstName+" "+student.LastName+"\n Doğum Tarihi: "+ student.BirthOfDate+" \n Ad Soyad: "+ student2.FirstName+" "+student2.LastName+ "\n Doğum Tarihi: "+student2.BirthOfDate );
